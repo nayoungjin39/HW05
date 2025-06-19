c4ab48752efe2342fbccf8514382fe4c8937423c
nayoungjin39 <211291869+nayoungjin39@users.noreply.github.com>
2025-06-19 11:12:16 +0900

    Create Text_Game.init.gen.cpp

---
+++ Text_Game/Intermediate/Build/Win64/UnrealEditor/Inc/Text_Game/UHT/Text_Game.init.gen.cpp
@@ -0,0 +1,29 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/GeneratedCppIncludes.h"
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+void EmptyLinkFunctionForGeneratedCodeText_Game_init() {}
+	static FPackageRegistrationInfo Z_Registration_Info_UPackage__Script_Text_Game;
+	FORCENOINLINE UPackage* Z_Construct_UPackage__Script_Text_Game()
+	{
+		if (!Z_Registration_Info_UPackage__Script_Text_Game.OuterSingleton)
+		{
+			static const UECodeGen_Private::FPackageParams PackageParams = {
+				"/Script/Text_Game",
+				nullptr,
+				0,
+				PKG_CompiledIn | 0x00000000,
+				0xA14C4349,
+				0x2A6997EB,
+				METADATA_PARAMS(0, nullptr)
+			};
+			UECodeGen_Private::ConstructUPackage(Z_Registration_Info_UPackage__Script_Text_Game.OuterSingleton, PackageParams);
+		}
+		return Z_Registration_Info_UPackage__Script_Text_Game.OuterSingleton;
+	}
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_Text_Game(Z_Construct_UPackage__Script_Text_Game, TEXT("/Script/Text_Game"), Z_Registration_Info_UPackage__Script_Text_Game, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0xA14C4349, 0x2A6997EB));
+PRAGMA_ENABLE_DEPRECATION_WARNINGS

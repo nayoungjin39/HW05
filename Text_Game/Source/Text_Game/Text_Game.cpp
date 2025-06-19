d98d3798ed545dd93350509698650b59140b32ee
nayoungjin39 <211291869+nayoungjin39@users.noreply.github.com>
2025-06-19 11:13:44 +0900

    Create Text_Game.cpp

---
+++ Text_Game/Source/Text_Game/Text_Game.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Text_Game.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Text_Game, "Text_Game" );
